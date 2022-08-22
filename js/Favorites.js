b9deea786593183c41e7b7d817f227ed0277142c
Rodrigo Braz <braz.rodrigo94@gmail.com>
2022-08-22 20:19:52 -0300

    Gravacao de dados no localStorage, busca, insercao e delecao de usuarios da lista, captura de erro caso o usuario nao seja encontrado

--- js/Favorites.js
+++ js/Favorites.js
@@ -27,14 +27,39 @@ export class Favorites {
             console.log(this.entries)
     }
 
+    // O método JSON.stringfy, converte o JSON em String para salvar os dados no localStorage
+    save() {
+        localStorage.setItem("@github-fav:", JSON.stringify(this.entries))
+    }
+
     /** Utilizando o async e await, criamos uma promisse,
      * Ela tem a mesma utilidade de quando usamos o fetch e vamos criando o passo
      * a passo do que precisamos
      * 
      * Este método busca o usuário na API do Github pelo userName ao clicar no botão de busca
      */
-    async add(userName) {
-        const githubUser = await GithubUser.search(userName)
+    async add(username) {
+        try {
+            const user = await GithubUser.search(username)
+
+            console.log(user)
+
+            if(user.login === undefined) {
+                throw new Error(`Usuário não encontrado!`)
+            }
+
+            /** ...this é um "spread operator", estamos utilizando para trazer
+             * os dados do array antigo para o novo, sem ferir a imutabilidade.
+             * Ele está inserindo o novo usuário e "espalhando" os que já estavam
+             * cadastrados para esse novo array também.
+             */
+            this.entries = [user, ...this.entries]
+            this.update()
+            this.save()
+
+        } catch(error) {
+            alert(error.message)
+        }
     }
 
     /** Ao utilizar o filter, não deletamos o array anterior e sim criamos um novo
@@ -44,10 +69,11 @@ export class Favorites {
      */
     delete(user) {
         const filteredEntries = this.entries
-            .filter(entry => entry.userName !== user.userName)
+            .filter(entry => entry.login !== user.login)
 
         this.entries = filteredEntries
         this.update()
+        this.save()
     }
 }
 
@@ -87,10 +113,10 @@ export class FavoritesView extends Favorites {
         this.entries.forEach(user => {
             const newRow = this.createRow()
 
-            newRow.querySelector('.user img').src = `https://github.com/${user.userName}.png`
+            newRow.querySelector('.user img').src = `https://github.com/${user.login}.png`
             newRow.querySelector('.user img').alt = `Imagem de ${user.name}`
             newRow.querySelector('.user p').textContent = user.name
-            newRow.querySelector('.user span').textContent = user.userName
+            newRow.querySelector('.user span').textContent = user.login
             newRow.querySelector('.repositories ').textContent = user.publicRepos
             newRow.querySelector('.followers').textContent = user.followers
 
@@ -101,7 +127,6 @@ export class FavoritesView extends Favorites {
                     this.delete(user)
                 }
             }
-
             // O método append, cria uma nova linha ao final do array
             this.tbody.append(newRow)
         })
